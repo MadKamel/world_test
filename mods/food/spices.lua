c54259c0c6455c30742836b9dc19cfae7826f8de
Eric Rustrum <rustrumek@gmail.com>
2023-03-21 14:49:46 -0700

    Removed new stuff
    The goal of the /main/ branch is to archive the old version of world_test. Not add stuff.

--- mods/food/spices.lua
+++ mods/food/spices.lua
@@ -14,75 +14,11 @@ minetest.register_node("food:rose_spice", {
 	groups = {dig_immediate = 3, falling_node = 1, flammable = 0},
 })
 
-minetest.register_node("food:black_tulip_spice", {
-	description = "Nightshade Grindings",
-	drawtype = "plantlike",
-	tiles = {"food_black_tulip_spice.png"},
-	inventory_image = "food_black_tulip_spice.png",
-	paramtype = "light",
-	sunlight_propagates = true,
-	walkable = false,
-	is_ground_content = false,
-	selection_box = {
-		type = "fixed",
-		fixed = {-3 / 16, -7 / 16, -3 / 16, 3 / 16, 4 / 16, 3 / 16}
-	},
-	groups = {dig_immediate = 3, falling_node = 1, flammable = 0},
-})
-
-minetest.register_node("food:tulip_spice", {
-	description = "Tulip Grindings",
-	drawtype = "plantlike",
-	tiles = {"food_tulip_spice.png"},
-	inventory_image = "food_tulip_spice.png",
-	paramtype = "light",
-	sunlight_propagates = true,
-	walkable = false,
-	is_ground_content = false,
-	selection_box = {
-		type = "fixed",
-		fixed = {-3 / 16, -7 / 16, -3 / 16, 3 / 16, 4 / 16, 3 / 16}
-	},
-	groups = {dig_immediate = 3, falling_node = 1, flammable = 0},
-})
-
 minetest.register_node("food:dandelion_spice", {
-	description = "Dandelion Pollen",
+	description = "Dandelion Spice",
 	drawtype = "plantlike",
 	tiles = {"food_dandelion_spice.png"},
 	inventory_image = "food_dandelion_spice.png",
-	paramtype = "light",
-	sunlight_propagates = true,
-	walkable = false,
-	is_ground_content = false,
-	selection_box = {
-		type = "fixed",
-		fixed = {-3 / 16, -7 / 16, -3 / 16, 3 / 16, 4 / 16, 3 / 16}
-	},
-	groups = {dig_immediate = 3, falling_node = 1, flammable = 0},
-})
-
-minetest.register_node("food:dandelion_seed", {
-	description = "Dandelion Floaters",
-	drawtype = "plantlike",
-	tiles = {"food_dandelion_seed.png"},
-	inventory_image = "food_dandelion_seed.png",
-	paramtype = "light",
-	sunlight_propagates = true,
-	walkable = false,
-	is_ground_content = false,
-	selection_box = {
-		type = "fixed",
-		fixed = {-3 / 16, -7 / 16, -3 / 16, 3 / 16, 4 / 16, 3 / 16}
-	},
-	groups = {dig_immediate = 3, falling_node = 1, flammable = 0},
-})
-
-minetest.register_node("food:dandelion_stalk", {
-	description = "Dandelion Stalks",
-	drawtype = "plantlike",
-	tiles = {"food_dandelion_stalks.png"},
-	inventory_image = "food_dandelion_stalk.png",
 	paramtype = "light",
 	sunlight_propagates = true,
 	walkable = false,
