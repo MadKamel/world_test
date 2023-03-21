9c369f9c1f7710cb1d369529e1153493c1b96563
Eric Rustrum <rustrumek@gmail.com>
2023-03-21 14:32:59 -0700

    Moved crafting definitions
    Now crafting definitions are stored in the file most relevant, rather than in the crafting.lua file. The old system was not very good.

--- mods/furniture/table.lua
+++ mods/furniture/table.lua
@@ -1,3 +1,5 @@
+# FIRST UP: TABLE DEFINITIONS
+
 minetest.register_node("furniture:wood_table", {
 	description = "Wooden Table",
 	tiles = {"default_wood.png"},
@@ -92,4 +94,44 @@ minetest.register_node("furniture:clay_table", {
 		},
 	},
 	groups = {dig_immediate = 2, falling_node = 1, flammable = 0},
+})
+
+
+
+# NEXT UP: CRAFTING DEFINITIONS
+
+minetest.register_craft({
+	output = "furniture:wood_table",
+	recipe = {
+		{'default:wood', 'default:wood', 'default:wood'},
+		{'group:stick', '', 'group:stick'},
+		{'group:stick', '', 'group:stick'},
+	}
+})
+
+minetest.register_craft({
+	output = "furniture:pine_wood_table",
+	recipe = {
+		{'default:pine_wood', 'default:pine_wood', 'default:pine_wood'},
+		{'group:stick', '', 'group:stick'},
+		{'group:stick', '', 'group:stick'},
+	}
+})
+
+minetest.register_craft({
+	output = "furniture:aspen_wood_table",
+	recipe = {
+		{'default:aspen_wood', 'default:aspen_wood', 'default:aspen_wood'},
+		{'group:stick', '', 'group:stick'},
+		{'group:stick', '', 'group:stick'},
+	}
+})
+
+minetest.register_craft({
+	output = "furniture:clay_table",
+	recipe = {
+		{'default:clay', 'default:clay', 'default:clay'},
+		{'group:stick', '', 'group:stick'},
+		{'group:stick', '', 'group:stick'},
+	}
 })
