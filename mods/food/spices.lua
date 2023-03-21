12a441b7cdee4e7d25a2ad5deba220204829fed2
Eric Rustrum <rustrumek@gmail.com>
2023-03-21 11:39:05 -0700

    Added dandelion stalks
    You gather these and can use them in recipes.

--- mods/food/spices.lua
+++ mods/food/spices.lua
@@ -77,3 +77,19 @@ minetest.register_node("food:dandelion_seed", {
 	},
 	groups = {dig_immediate = 3, falling_node = 1, flammable = 0},
 })
+
+minetest.register_node("food:dandelion_stalk", {
+	description = "Dandelion Stalks",
+	drawtype = "plantlike",
+	tiles = {"food_dandelion_stalks.png"},
+	inventory_image = "food_dandelion_stalk.png",
+	paramtype = "light",
+	sunlight_propagates = true,
+	walkable = false,
+	is_ground_content = false,
+	selection_box = {
+		type = "fixed",
+		fixed = {-3 / 16, -7 / 16, -3 / 16, 3 / 16, 4 / 16, 3 / 16}
+	},
+	groups = {dig_immediate = 3, falling_node = 1, flammable = 0},
+})
