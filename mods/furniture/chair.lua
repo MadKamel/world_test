772909a33d2fd87516a3fab223f1c7153993dfce
Eric Rustrum <rustrumek@gmail.com>
2023-03-21 13:57:06 -0700

    Added more chairs
    3 kinds of wood, clay

--- mods/furniture/chair.lua
+++ mods/furniture/chair.lua
@@ -22,3 +22,78 @@ minetest.register_node("furniture:wood_chair", {
 	},
 	groups = {dig_immediate = 2, falling_node = 1, flammable = 1},
 })
+
+minetest.register_node("furniture:aspen_wood_chair", {
+	description = "Aspen Chair",
+	tiles = {"default_aspen_wood.png"},
+	drawtype = "nodebox",
+	sunlight_propagates = true,
+	paramtype = 'light',
+	paramtype2 = "facedir",
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{-0.3,-0.5,0.2, -0.2,0.5,0.3},
+			{0.2,-0.5,0.2, 0.3,0.5,0.3},
+			{-0.3,-0.5,-0.3, -0.2,-0.1,-0.2},
+			{0.2,-0.5,-0.3, 0.3,-0.1,-0.2},
+			{-0.3,-0.1,-0.3, 0.3,0,0.2},
+			{-0.2,0.1,0.25, 0.2,0.4,0.26}
+		},
+	},
+	selection_box = {
+		type = "fixed",
+		fixed = {-0.3, -0.5, -0.3, 0.3, 0.5, 0.3},
+	},
+	groups = {dig_immediate = 2, falling_node = 1, flammable = 1},
+})
+
+minetest.register_node("furniture:pine_wood_chair", {
+	description = "Pine Chair",
+	tiles = {"default_pine_wood.png"},
+	drawtype = "nodebox",
+	sunlight_propagates = true,
+	paramtype = 'light',
+	paramtype2 = "facedir",
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{-0.3,-0.5,0.2, -0.2,0.5,0.3},
+			{0.2,-0.5,0.2, 0.3,0.5,0.3},
+			{-0.3,-0.5,-0.3, -0.2,-0.1,-0.2},
+			{0.2,-0.5,-0.3, 0.3,-0.1,-0.2},
+			{-0.3,-0.1,-0.3, 0.3,0,0.2},
+			{-0.2,0.1,0.25, 0.2,0.4,0.26}
+		},
+	},
+	selection_box = {
+		type = "fixed",
+		fixed = {-0.3, -0.5, -0.3, 0.3, 0.5, 0.3},
+	},
+	groups = {dig_immediate = 2, falling_node = 1, flammable = 1},
+})
+
+minetest.register_node("furniture:clay_chair", {
+	description = "Carved Chair",
+	tiles = {"default_clay.png"},
+	drawtype = "nodebox",
+	sunlight_propagates = true,
+	paramtype = 'light',
+	paramtype2 = "facedir",
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{-0.3,-0.5,0.2, -0.2,0.5,0.3},
+			{0.2,-0.5,0.2, 0.3,0.5,0.3},
+			{-0.3,-0.5,-0.3, -0.2,-0.1,-0.2},
+			{0.2,-0.5,-0.3, 0.3,-0.1,-0.2},
+			{-0.3,-0.1,-0.3, 0.3,0,0.2},
+			{-0.2,0.1,0.25, 0.2,0.4,0.26}
+		},
+	},
+	selection_box = {
+		type = "fixed",
+		fixed = {-0.3, -0.5, -0.3, 0.3, 0.5, 0.3},
+	},
+	groups = {dig_immediate = 2, falling_node = 1, flammable = 0},
+})
