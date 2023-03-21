06005ada995db9cedd8d57f2d06bbda90f802658
Eric Rustrum <rustrumek@gmail.com>
2023-03-21 11:34:46 -0700

    Added two spices.
    Black tulip (Nightshade) and Tulip. Renamed Dandelion spice to Dandelion Pollen.

--- mods/food/spices.lua
+++ mods/food/spices.lua
@@ -14,8 +14,40 @@ minetest.register_node("food:rose_spice", {
 	groups = {dig_immediate = 3, falling_node = 1, flammable = 0},
 })
 
+minetest.register_node("food:black_tulip_spice", {
+	description = "Nightshade Spice",
+	drawtype = "plantlike",
+	tiles = {"food_black_tulip_spice.png"},
+	inventory_image = "food_black_tulip_spice.png",
+	paramtype = "light",
+	sunlight_propagates = true,
+	walkable = false,
+	is_ground_content = false,
+	selection_box = {
+		type = "fixed",
+		fixed = {-3 / 16, -7 / 16, -3 / 16, 3 / 16, 4 / 16, 3 / 16}
+	},
+	groups = {dig_immediate = 3, falling_node = 1, flammable = 0},
+})
+
+minetest.register_node("food:tulip_spice", {
+	description = "Tulip Spice",
+	drawtype = "plantlike",
+	tiles = {"food_tulip_spice.png"},
+	inventory_image = "food_tulip_spice.png",
+	paramtype = "light",
+	sunlight_propagates = true,
+	walkable = false,
+	is_ground_content = false,
+	selection_box = {
+		type = "fixed",
+		fixed = {-3 / 16, -7 / 16, -3 / 16, 3 / 16, 4 / 16, 3 / 16}
+	},
+	groups = {dig_immediate = 3, falling_node = 1, flammable = 0},
+})
+
 minetest.register_node("food:dandelion_spice", {
-	description = "Dandelion Spice",
+	description = "Dandelion Pollen",
 	drawtype = "plantlike",
 	tiles = {"food_dandelion_spice.png"},
 	inventory_image = "food_dandelion_spice.png",
