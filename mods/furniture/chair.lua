e83298b6506cc281ea666934822a7fff0f669637
Eric Rustrum <rustrumek@gmail.com>
2023-03-21 11:20:25 -0700

    Made chairs tougher.

--- mods/furniture/chair.lua
+++ mods/furniture/chair.lua
@@ -20,5 +20,5 @@ minetest.register_node("furniture:wood_chair", {
 		type = "fixed",
 		fixed = {-0.3, -0.5, -0.3, 0.3, 0.5, 0.3},
 	},
-	groups = {dig_immediate = 3, falling_node = 1, flammable = 1},
+	groups = {dig_immediate = 2, falling_node = 1, flammable = 1},
 })
