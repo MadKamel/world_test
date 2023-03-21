c3e46b187cd929e5982aac0b2890f495636f9b80
Eric Rustrum <rustrumek@gmail.com>
2023-03-21 13:55:31 -0700

    Added more staves
    3 kinds of wood, and then clay.

--- mods/furniture/staff.lua
+++ mods/furniture/staff.lua
@@ -14,3 +14,54 @@ minetest.register_node("furniture:staff", {
 	groups = {dig_immediate = 2, flammable = 1},
 	on_place = minetest.rotate_node,
 })
+
+minetest.register_node("furniture:pine_staff", {
+	description = "Staff",
+	tiles = {"default_pine_wood.png", "default_pine_wood.png", "default_pine_wood.png"},
+	drawtype = "nodebox",
+	paramtype = "light",
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{-0.125, -0.5, -0.1875, 0.125, 0.5, 0.1875},
+			{-0.1875, -0.5, -0.125, 0.1875, 0.5, 0.125},
+		},
+	},
+	paramtype2 = "facedir",
+	groups = {dig_immediate = 2, flammable = 1},
+	on_place = minetest.rotate_node,
+})
+
+minetest.register_node("furniture:aspen_staff", {
+	description = "Staff",
+	tiles = {"default_aspen_wood.png", "default_aspen_wood.png", "default_aspen_wood.png"},
+	drawtype = "nodebox",
+	paramtype = "light",
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{-0.125, -0.5, -0.1875, 0.125, 0.5, 0.1875},
+			{-0.1875, -0.5, -0.125, 0.1875, 0.5, 0.125},
+		},
+	},
+	paramtype2 = "facedir",
+	groups = {dig_immediate = 2, flammable = 1},
+	on_place = minetest.rotate_node,
+})
+
+minetest.register_node("furniture:clay_staff", {
+	description = "Staff",
+	tiles = {"default_clay.png", "default_clay.png", "default_clay.png"},
+	drawtype = "nodebox",
+	paramtype = "light",
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{-0.125, -0.5, -0.1875, 0.125, 0.5, 0.1875},
+			{-0.1875, -0.5, -0.125, 0.1875, 0.5, 0.125},
+		},
+	},
+	paramtype2 = "facedir",
+	groups = {dig_immediate = 2, flammable = 0},
+	on_place = minetest.rotate_node,
+})
